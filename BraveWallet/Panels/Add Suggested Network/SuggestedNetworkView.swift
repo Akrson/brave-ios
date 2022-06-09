1e5310a53fc3110b36730a906e3b79f26a66d4f5
Nuo Xu <nuoxu.nx@gmail.com>
2022-06-09 16:25:42 -0400

    add context menu to reveal full account address

--- BraveWallet/Panels/Add Suggested Network/SuggestedNetworkView.swift
+++ BraveWallet/Panels/Add Suggested Network/SuggestedNetworkView.swift
@@ -116,13 +116,22 @@ struct SuggestedNetworkView: View {
   
   private var headerView: some View {
     VStack {
-      HStack(spacing: 8) {
-        Spacer()
+      Menu {
         Text(keyringStore.selectedAccount.address)
-          .fontWeight(.semibold)
-        Blockie(address: keyringStore.selectedAccount.address)
-          .frame(width: min(blockieSize, maxBlockieSize), height: min(blockieSize, maxBlockieSize))
-          .aspectRatio(1, contentMode: .fit)
+        Button(action: {
+          UIPasteboard.general.string = keyringStore.selectedAccount.address
+        }) {
+          Label(Strings.Wallet.copyAddressButtonTitle, image: "brave.clipboard")
+        }
+      } label: {
+        HStack(spacing: 8) {
+          Spacer()
+          Text(keyringStore.selectedAccount.address.truncatedAddress)
+            .fontWeight(.semibold)
+          Blockie(address: keyringStore.selectedAccount.address)
+            .frame(width: min(blockieSize, maxBlockieSize), height: min(blockieSize, maxBlockieSize))
+            .aspectRatio(1, contentMode: .fit)
+        }
       }
       .accessibilityLabel(Strings.Wallet.selectedAccountAccessibilityLabel)
       .accessibilityValue("\(keyringStore.selectedAccount.name), \(keyringStore.selectedAccount.address)")
@@ -193,6 +202,7 @@ struct SuggestedNetworkView: View {
       .opacity(sizeCategory.isAccessibilityCategory ? 0 : 1)
       .accessibility(hidden: sizeCategory.isAccessibilityCategory)
     }
+    .listStyle(InsetGroupedListStyle())
     .navigationTitle(navigationTitle)
     .navigationBarTitleDisplayMode(.inline)
     .overlay(
